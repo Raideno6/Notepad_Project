117ed3c76c4c3259031c316ff9f730351626ba70
Raideno6 <116860736+Raideno6@users.noreply.github.com>
2024-04-16 14:10:58 -0400

    Added second page to app

--- QuestTrack/app/src/main/java/com/example/questtrack/LogScreen.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/LogScreen.java
@@ -1,30 +1,23 @@
 package com.example.questtrack;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-
-
-public class MainActivity extends AppCompatActivity {
+public class LogScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    Button logBtn = (Button)findViewById(R.id.newButton);
-    
 }
