fd884cba5638e587beb81ccceb7a9c28345ee935
Raideno6 <116860736+Raideno6@users.noreply.github.com>
2024-05-03 12:38:46 -0400

    Started andorid stuido project

--- QuestTrack/app/src/main/java/com/example/questtrack/MainActivity.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.questtrack;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,18 +14,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button logBtn = findViewById(R.id.newButton);
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LogView.class);
+                startActivity(intent);
+            }
         });
     }
 
-    String newButton = "New Quest";
 }
