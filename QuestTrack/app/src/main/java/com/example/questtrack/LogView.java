580a6d2547ee67ac66f9d46da581e1eeaee87b95
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2024-05-09 14:16:12 -0400

    IT WORKS!!! LOADING NOTES

--- QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
@@ -12,7 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileNotFoundException;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class LogView extends AppCompatActivity {
+
+    Note currentNote = new Note("new note");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,27 @@ public class LogView extends AppCompatActivity {
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_view);
 
-        EditText editText = findViewById(R.id.noteName);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+             @Override public void run() {
+                 try {
+                     Note note = DataHandler.getNote(getIntent().getStringExtra("note"), LogView.this);
+                     if (note != null) {
+                         currentNote = note;
+                     }
+                     EditText noteName = findViewById(R.id.noteName);
+                     noteName.setText(currentNote.getNoteName());
+                     EditText noteBody = findViewById(R.id.noteBody);
+                     noteBody.setText(currentNote.getBody());
+                 } catch (FileNotFoundException | IllegalAccessException e) {
+                     throw new RuntimeException(e);
+                 }
+             }
+        });
+
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.logScreen), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
