3014cf899fd8daec224a5a0462654708411d600c
Raymond Spitz <116860736+Raideno6@users.noreply.github.com>
2024-05-09 12:09:56 -0400

    Added file path to DataHandler

--- QuestTrack/app/src/main/java/com/example/questtrack/DataHandler.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/DataHandler.java
@@ -5,9 +5,16 @@ import android.os.Build;
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -16,19 +23,16 @@ import java.util.ArrayList;
 
 public class DataHandler {
 
-    public static void SaveNotes(String title, String body){
-        Note temp = new Note(title);
-        temp.setBody(body);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            temp.setDate(LocalDateTime.now().toString());
-        }
-
-        File directory;
-
-        if (filename.isEmpty()) {
-            directory = getFilesDir;
+    public static void SaveNote(Context context, Note note) throws IOException {
+        File directory = new File("/data/data/com.example.questtrack/files/Notes");
+        File file = new File(directory, note.getNoteName());
+        String fileContent = new Gson().toJson(note);
+        byte[] byteArrray = fileContent.getBytes();
+        try (FileOutputStream fos = context.openFileOutput(file.getName(), Context.MODE_PRIVATE)) {
+            fos.write(byteArrray);
         }
     }
+
 
     public static Note convertJsonToObject(Context context, int rawID) {
 
@@ -48,6 +52,8 @@ public class DataHandler {
     }
 
     public static ArrayList<Note> getAllNotes(Context context) throws IllegalAccessException {
+        String[] files = context.fileList();
+
         ArrayList<Note> test = new ArrayList<>();
         Field[] fields = R.raw.class.getFields();
         for(int i=0; i < fields.length; i++){
