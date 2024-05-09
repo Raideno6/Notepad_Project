3eae2bbdb38db252763fae032627fa78e81e643d
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2024-05-09 12:35:54 -0400

    added save btn

--- QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
@@ -1,6 +1,9 @@
 package com.example.questtrack;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 // androidx.activity.EdgeToEdge;
@@ -25,6 +28,15 @@ public class LogView extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button saveBtn = findViewById(R.id.saveButton);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
 
 
