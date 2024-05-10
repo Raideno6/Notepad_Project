c1d11c1eefc3e8665cb4dc919d789a9a9f8652fb
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2024-05-10 12:12:35 -0400

    Update LogView.java

--- QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
+++ QuestTrack/app/src/main/java/com/example/questtrack/LogView.java
@@ -29,26 +29,20 @@ public class LogView extends AppCompatActivity {
         setContentView(R.layout.activity_log_view);
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.execute(new Runnable() {
-             @Override public void run() {
-                 try {
-                     Note note = DataHandler.getNote(getIntent().getStringExtra("note"), LogView.this);
-                     if (note != null) {
-                         currentNote = note;
-                     }
-                     EditText noteName = findViewById(R.id.noteName);
-                     noteName.setText(currentNote.getNoteName());
-                     EditText noteBody = findViewById(R.id.noteBody);
-                     noteBody.setText(currentNote.getBody());
-                 } catch (FileNotFoundException | IllegalAccessException e) {
-                     throw new RuntimeException(e);
-                 }
-             }
+        executor.execute(() -> {
+            try {
+                Note note = DataHandler.getNote(getIntent().getStringExtra("note"), LogView.this);
+                if (note != null) {
+                    currentNote = note;
+                }
+                EditText noteName = findViewById(R.id.noteName);
+                noteName.setText(currentNote.getNoteName());
+                EditText noteBody = findViewById(R.id.noteBody);
+                noteBody.setText(currentNote.getBody());
+            } catch (FileNotFoundException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         });
-
-
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.logScreen), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
